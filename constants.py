8eb27b1eba85291503973acf262b8c58d0c29149
supperdoggy <49492887+supperdoggy@users.noreply.github.com>
2019-11-21 17:53:37 +0200

    Delete constants.py

--- constants.py
+++
@@ -1,7 +0,0 @@
-TODO_MODEL_NUMBER = 1
-INPROCESS_MODEL_NUMBER = 2
-DONE_MODEL_NUMBER = 3
-NOTES_MODEL_NUMBER = 4
-DATABASE_URL = 'postgres://dqdabdevxljkyi:c2656aada50f2886f816416e9eda0ffe230a1a961d51fb0bf84a985c82f54eea@ec2-174-129-253-45.compute-1.amazonaws.com:5432/d6q0u2trvgkri7'
-secret_key = "KbV5qPvUtn6Og4jZBiLbNMQIS"
-
